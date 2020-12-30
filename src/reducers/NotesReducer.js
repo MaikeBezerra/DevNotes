52d6a486170785426baf9d1d092580275eec4bb6
MaikeBezerra <maike_ti@hotmail.com>
2020-12-30 18:13:48 -0300

    Configuration redux

---
+++ src/reducers/NotesReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+    list: [
+        { title: 'First note', body: 'Testing'}
+    ]
+}
+
+export default (state = initialState, action) => {
+    switch(action.type) {
+        case 'ADD_NOTE':
+            break;
+    }
+    
+    return state;
+}
